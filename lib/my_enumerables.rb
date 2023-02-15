936807483981ddcb969ea831bc13442df7eb5177
Jesus <jesuswrivas@gmail.com>
2023-02-15 11:29:13 -0500

    my_each method created
    Create my_each method on Array class.
    
    Create my_all, my_any, my_count methods. All fully functional

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,56 @@
 module Enumerable
-  # Your code goes here
+
+  def my_count(&block)
+
+    if block_given?
+      counter = 0
+      self.my_each do |x|
+      counter += 1 if block.call(x)
+      end
+      
+      return counter
+    else
+      return self.length
+    end
+  end
+    
+
+  def my_all?(&block)
+  aux_array = []  
+    self.my_each do |x|
+    aux_array << block.call(x)
+    end
+  return aux_array.uniq.length == 1 && aux_array[0] == true
+  end
+
+  def my_any?(&block)
+   
+      self.my_each do |x|
+      return true if block.call(x)
+      
+      return false
+      end
+  end
+
 end
+
 
 # You will first have to define my_each
 # on the Array class. Methods defined in
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
-end
+  include Enumerable
+
+  def my_each 
+    for i in 0..self.length-1 do
+      yield (self[i])    
+    end
+    return self
+  end
+ end
+
+
+
+# newArray = [1,2,3,4,1,2,6,4]
+ #puts newArray.my_count
